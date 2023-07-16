871adaef20ddf7f03ac7672d500e2bf1d1493cdc
Pegah Kazemi <p.kazemi@Rayan.Parsi>
2023-07-16 15:38:45 +0330

    update counter

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <section class="text-3xl flex justify-center flex-col mx-auto text-center"></section>
+        <section class="text-3xl flex justify-center flex-col mx-auto text-center">Until:</section>
         <section class="flex text-6xl justify-center content-center">
             <div class="days mr-2 relative">
                 {{ displayDays }}
@@ -44,10 +44,52 @@ export default {
             displaySeconds: 0,
         }
     },
+    computed: {
+        _seconds: () => 1000,
+        _minutes() {
+            return this._seconds * 60
+        },
+        _hours() {
+            return this._minutes * 60
+        },
+        _days() {
+            return this._hours * 24
+        }
+    },
+    mounted() {
+        this.showRemaining();
+    },
+    methods: {
+        formatNum: num => (num < 10 ? '0' + num : num),
+        showRemaining() {
+            const timer = setInterval(() => {
+                const now = new Date()
+                const end = new Date(2023, 7, 16, 15, 46, 0)
+                console.log(end.getTime(),'end')
+                console.log(now.getTime(),'now')
+                const distance = end.getTime() - now.getTime()
+                if (distance < 0) {
+                    clearInterval(timer)
+                    return
+                }
+
+                const days = Math.floor((distance / this._days))
+                const hours = Math.floor((distance % this._days) / this._hours)
+                const minutes = Math.floor((distance % this._hours) / this._minutes)
+                const seconds = Math.floor((distance % this._minutes) / this._seconds)
+
+                this.displaySeconds = this.formatNum(seconds)
+                this.displayMinutes = this.formatNum(minutes)
+                this.displayHours = this.formatNum(hours)
+                this.displayDays = this.formatNum(days)
+            },1000)
+        }
+    },
 }
 </script>
 
-
 <style scoped>
-
+.seconds {
+    max-width: 60px;
+}
 </style>
