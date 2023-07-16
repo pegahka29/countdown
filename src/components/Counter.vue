e1f2ee06327041832b54c7d3e27a266278cc8e16
Pegah Kazemi <p.kazemi@Rayan.Parsi>
2023-07-16 14:20:46 +0330

    add counter component

---
+++ src/components/Counter.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <section class="text-3xl flex justify-center flex-col mx-auto text-center"></section>
+        <section class="flex text-6xl justify-center content-center">
+            <div class="days mr-2 relative">
+                {{ displayDays }}
+                <div class="label text-sm absolute -bottom-3 ">Days</div>
+            </div>
+            <span class="landing-snug">
+                :
+            </span>
+            <div class="hours mx-2 relative">
+                {{ displayHours }}
+                <div class="label text-sm absolute -bottom-3">Hours</div>
+            </div>
+            <span class="landing-snug">
+                :
+            </span>
+            <div class="minutes mr-2 relative">
+                {{ displayMinutes }}
+                <div class="label text-sm absolute -bottom-3">Minutes</div>
+            </div>
+            <span class="landing-snug">
+                :
+            </span>
+            <div class="seconds mr-2 relative">
+                {{ displaySeconds }}
+                <div class="label text-sm absolute -bottom-3">Seconds</div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+
+export default {
+    //eslint-disable-next-line
+    name: "Counter",
+    data() {
+        return {
+            displayDays: 0,
+            displayHours: 0,
+            displayMinutes: 0,
+            displaySeconds: 0,
+        }
+    },
+}
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <counter/>
+</template>
+
+<script>
+
+import Counter from "@/components/Counter.vue";
+
+export default {
+
+    //eslint-disable-next-line
+    name: "Home",
+    components: {Counter}
+}
+</script>
+
+<style scoped>
+
+</style>
